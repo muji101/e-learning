2614c4af0f23027799265dc14cda1b3c50011c13
muji101 <mujikuwat91@gmail.com>
2021-07-30 08:33:45 +0700

    front dinamis

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,13 @@ use Illuminate\Support\Facades\Route;
 // route home
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
+// route category
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
 Route::get('/categories/{name}', [CategoryController::class, 'detail'])->name('categories-detail');
+
+// route search
+Route::get('/search', [SearchController::class, 'search'])->name('search');
+Route::get('/search/{id}', [SearchController::class, 'searchCategory'])->name('search-category');
 
 // Route class front
 // Route::get('/class', [ClassController::class, 'index'])->name('class_list');
@@ -40,8 +45,6 @@ Route::middleware(['auth'])->group(function () {
 });
 
 
-Route::get('/search', [SearchController::class, 'search'])->name('search');
-Route::get('/search/{id}', [SearchController::class, 'searchCategory'])->name('search-category');
 
 
 // middleware_default
@@ -92,61 +95,3 @@ Route::prefix('/dashboard')->middleware(['auth'])->group(function () {
     Route::delete('/category/delete/{id}', [DashboardCategoryController::class, 'destroy'])->name('category-delete');
 
 });
-
-
-// Route::get('/', 'HomeController@index')->name('home');
-
-// Route::get('/categories', 'CategoryController@index')->name('categories');
-// Route::get('/categories/{id}', 'CategoryController@detail')->name('categories-detail');
-
-// Route::get('/details/{id}', 'DetailController@index')->name('detail');
-// Route::post('/details/{id}', 'DetailController@add')->name('detail-add');
-
-
-
-// Route::post('/checkout/callback', 'CheckoutController@callback')->name('midtrans-callback');
-
-// Route::get('/success', 'CartController@success')->name('success');
-
-// Route::get('/register/success', 'Auth\RegisterController@success')->name('register-success');
-
-
-// example
-// Route::group(['middleware' => ['auth']], function (){
-//     Route::get('/cart', 'CartController@index')->name('cart');
-//     Route::delete('/cart/{id}', 'CartController@delete')->name('cart-delete');
-
-//     Route::post('/checkout', 'CheckoutController@process')->name('checkout');
-
-//     Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
-
-//     Route::get('/dashboard/products', 'DashboardProductController@index')->name('dashboard-product');
-//     Route::post('/dashboard/products', 'DashboardProductController@store')->name('dashboard-product-store');
-//     Route::get('/dashboard/products/create', 'DashboardProductController@create')->name('dashboard-product-create');
-//     Route::get('/dashboard/products/{id}', 'DashboardProductController@details')->name('dashboard-product-details');
-//     Route::post('/dashboard/products/{id}', 'DashboardProductController@update')->name('dashboard-product-update');
-
-//     Route::post('/dashboard/products/gallery/upload', 'DashboardProductController@uploadGallery')->name('dashboard-product-gallery-upload');
-//     Route::get('/dashboard/products/gallery/delete/{id}', 'DashboardProductController@deleteGallery')->name('dashboard-product-gallery-delete');
-
-//     Route::get('/dashboard/transactions', 'DashboardTransactionController@index')->name('dashboard-transaction');
-//     Route::get('/dashboard/transactions/{id}', 'DashboardTransactionController@details')->name('dashboard-transactions-details');
-//     Route::post('/dashboard/transactions/{id}', 'DashboardTransactionController@update')->name('dashboard-transactions-update');
-
-//     Route::get('/dashboard/settings', 'DashboardSettingController@store')->name('dashboard-settings-store');
-//     Route::get('/dashboard/account', 'DashboardSettingController@account')->name('dashboard-settings-account');
-//     Route::post('/dashboard/account/{redirect}', 'DashboardSettingController@update')->name('dashboard-settings-redirect');
-
-// });
-
-// Route::prefix('admin')
-//     ->namespace('Admin')
-//     ->middleware(['auth','admin'])
-//     ->group(function(){
-//         Route::get('/', 'DashboardController@index')->name('admin-dashboard');
-//         Route::resource('category', 'CategoryController');
-//         Route::resource('user', 'UserController');
-//         Route::resource('product', 'ProductController');
-//         Route::resource('product-gallery', 'ProductGalleryController');
-//         Route::resource('transaction', 'TransactionController');
-// });
