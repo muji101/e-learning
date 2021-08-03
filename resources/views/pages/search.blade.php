619440a64dffd53ee34f99c67d554047ad1790c8
muji101 <mujikuwat91@gmail.com>
2021-08-03 21:59:20 +0700

    fix bug & fix design

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -16,7 +16,7 @@
                 <!-- view with category -->
                 <div class="sticky top-20 flex flex-col text-xl font-bold gap-8 text-white pt-8">
                     <a 
-                    href="/search"
+                    href="{{ route('search') }}"
                     class="bg-gray-500 py-2 px-4 rounded-lg {{ (request()->is('search')) ? 'bg-gradient-to-r from-blue-900 to to-blue-400' : '' }}"
                     >Semua</a>
                     {{-- <a data-aos="fade-right" data-aos-delay="400" href="{{ route('categories') }}">Semua</a> --}}
