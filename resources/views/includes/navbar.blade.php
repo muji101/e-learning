e5f81133c9e79e9990f1faa89b64abf8d9c64c0b
muji101 <mujikuwat91@gmail.com>
2021-08-05 10:03:08 +0700

    fix bug

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -36,7 +36,7 @@
                 <div @click.away="open = false" class="relative flex" x-data="{ open: false }">
                     <button @click="open = !open" class="flex flex-row items-center w-full px-1 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
                         <div class="flex items-center space-x-4">
-                                <img src="{{ asset($image ? '/storage/'. Auth::user()->image : '/images/avatar.png' ) }}" alt="" class="border-2 rounded-full" style="width: 40px ; height: 40px">
+                                <img src="{{ asset($image ? '/storage/'. Auth::user()->image : asset('/images/profile.svg') ) }}" alt="" class="border-2 rounded-full" style="width: 40px ; height: 40px">
                                 <p>Hi, {{ Auth::user()->name }}</p>
                             <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         </div>
